ea8166b49639c74363a676385700e7436fd3c65a
luckylooke <luckylooke@gmail.com>
2015-06-25 22:29:07 +0200

    Simplified directive

--- dragular.js
+++ dragular.js
@@ -551,22 +551,7 @@ angular.module('dragularModule', []).factory('dragularService', function dragula
   }
 
 }).directive('dragular', function(dragularService) {
-  // Runs during compile
-  return {
-    // name: '',
-    // priority: 1,
-    // terminal: true,
-    // scope: {}, // {} = isolate, true = child, false/undefined = no change
-    // controller: function($scope, $element, $attrs, $transclude) {},
-    // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-    // restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
-    // template: '',
-    // templateUrl: '',
-    // replace: true,
-    // transclude: true,
-    // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
-    link: function($scope, iElm, iAttrs) {
-      dragularService(iElm[0], $scope[iAttrs.dragular || 'undefined']);
-    }
+  return function($scope, iElm, iAttrs) {
+    dragularService(iElm[0], $scope[iAttrs.dragular || 'undefined']);
   };
 });
