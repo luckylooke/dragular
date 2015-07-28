62fc4aa7353420d179c8f194882b86bf6037bc6a
luckylooke <luckylooke@gmail.com>
2015-07-28 21:53:43 +0200

    preparation for loading examples into main view

--- docs/src/examples/examplesApp.js
+++ docs/src/examples/examplesApp.js
@@ -6,4 +6,58 @@
  *
  *  DEMO app for dragular https://github.com/luckylooke/dragular
  */
-angular.module('examplesApp', ['dragularModule']);
+angular.module('examplesApp', ['dragularModule']).controller('ExAppCtrl', ['$rootScope', function($rootScope) {
+    $scope.examplesList = [{
+        template: 'exampleBasic',
+        title: 'Basic'
+    }, {
+        template: 'exampleDirective',
+        title: 'Directive'
+    }, {
+        template: 'exampleEvents',
+        title: 'Events'
+    }, {
+        template: 'exampleRemoveOnSpill',
+        title: 'Remove on spill'
+    }, {
+        template: 'exampleRevertOnSpill',
+        title: 'Revert on spill'
+    }, {
+        template: 'exampleCopy',
+        title: 'Copy'
+    }, {
+        template: 'exampleHandle',
+        title: 'Handle'
+    }, {
+        template: 'exampleCustomClasses',
+        title: 'Custom classes'
+    }, {
+        template: 'exampleNameSpaces',
+        title: 'NameSpaces'
+    }, {
+        template: 'exampleDragOverClasses',
+        title: 'DragOver classes'
+    }, {
+        template: 'exampleBoundingBox',
+        title: 'BoundingBox'
+    }, {
+        template: 'exampleBoundingBoxLockX',
+        title: 'BoundingBox + LockX'
+    }, {
+        template: 'exampleBoundingBoxLockY',
+        title: 'BoundingBox + LockY'
+    }, {
+        template: 'exampleNgRepeat',
+        title: 'ngRepeat'
+    }, {
+        template: 'exampleNestedNgRepeat',
+        title: 'Nested ngRepead'
+    }, {
+        template: 'exampleScrollingDrag',
+        title: 'Scrolling drag'
+    }];
+    $rootScope.models = false;
+    $scope.toggleModels = function toggleModels() {
+        $rootScope.models = !$rootScope.models;
+    }
+}]);
