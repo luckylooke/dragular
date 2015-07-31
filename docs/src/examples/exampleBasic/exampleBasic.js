02cc402c3b1c92c770cdc58c9800fda165d4acf7
luckylooke <luckylooke@gmail.com>
2015-07-31 14:15:43 +0200

    toggling model binded examples

--- docs/src/examples/exampleBasic/exampleBasic.js
+++ docs/src/examples/exampleBasic/exampleBasic.js
@@ -13,6 +13,28 @@ examplesAppModule
   }]);
 
 examplesAppModule
-  .controller('BasicModel', ['$element', 'dragularService', function TodoCtrl($element, dragularService) {
-    dragularService($element.children());
+  .controller('BasicModel', ['$scope', '$element', 'dragularService', function TodoCtrl($scope, $element, dragularService) {
+    $scope.items1 = [{
+      content: 'Move me, but you can only drop me in one of these containers.'
+    }, {
+      content: 'If you try to drop me somewhere other than these containers, I\'ll just come back.'
+    }, {
+      content: 'Item 3'
+    }, {
+      content: 'Item 4'
+    }];
+    $scope.items2 = [{
+      content: 'Item 5'
+    }, {
+      content: 'Item 6'
+    }, {
+      content: 'Item 7'
+    }, {
+      content: 'Item 8'
+    }];
+    console.log($scope.globals);
+    var containers = $element.children().children();
+    dragularService([containers[0],containers[1]],{
+      containersModel: [$scope.items1, $scope.items2]
+    });
   }]);
--- docs/src/examples/examplesApp.js
+++ docs/src/examples/examplesApp.js
@@ -14,61 +14,59 @@ require('./templates');
  */
 
 module.exports = angular.module('examplesApp', ['dragularModule', 'templates']).controller('ExAppCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
-  $scope.examplesList = [{
-    template: 'exampleBasic/exampleBasic.html',
-    title: 'Basic'
-  }, {
-    template: 'exampleDirective/exampleDirective.html',
-    title: 'Directive'
-  }, {
-    template: 'exampleEvents/exampleEvents.html',
-    title: 'Events'
-  }, {
-    template: 'exampleRemoveOnSpill/exampleRemoveOnSpill.html',
-    title: 'Remove on spill'
-  }, {
-    template: 'exampleRevertOnSpill/exampleRevertOnSpill.html',
-    title: 'Revert on spill'
-  }, {
-    template: 'exampleCopy/exampleCopy.html',
-    title: 'Copy'
-  }, {
-    template: 'exampleHandle/exampleHandle.html',
-    title: 'Handle'
-  }, {
-    template: 'exampleCustomClasses/exampleCustomClasses.html',
-    title: 'Custom classes'
-  }, {
-    template: 'exampleNameSpaces/exampleNameSpaces.html',
-    title: 'NameSpaces'
-  }, {
-    template: 'exampleDragOverClasses/exampleDragOverClasses.html',
-    title: 'DragOver classes'
-  }, {
-    template: 'exampleBoundingBox/exampleBoundingBox.html',
-    title: 'BoundingBox'
-  }, {
-    template: 'exampleBoundingBoxLockX/exampleBoundingBoxLockX.html',
-    title: 'BoundingBox + LockX'
-  }, {
-    template: 'exampleBoundingBoxLockY/exampleBoundingBoxLockY.html',
-    title: 'BoundingBox + LockY'
-  }, {
-    template: 'exampleNgRepeat/exampleNgRepeat.html',
-    title: 'ngRepeat'
-  }, {
-    template: 'exampleNestedNgRepeat/exampleNestedNgRepeat.html',
-    title: 'Nested ngRepead'
-  }, {
-    template: 'exampleScrollingDrag/exampleScrollingDrag.html',
-    title: 'Scrolling drag'
-  }];
+    $scope.examplesList = [{
+        template: 'exampleBasic/exampleBasic.html',
+        title: 'Basic'
+    }, {
+        template: 'exampleDirective/exampleDirective.html',
+        title: 'Directive'
+    }, {
+        template: 'exampleEvents/exampleEvents.html',
+        title: 'Events'
+    }, {
+        template: 'exampleRemoveOnSpill/exampleRemoveOnSpill.html',
+        title: 'Remove on spill'
+    }, {
+        template: 'exampleRevertOnSpill/exampleRevertOnSpill.html',
+        title: 'Revert on spill'
+    }, {
+        template: 'exampleCopy/exampleCopy.html',
+        title: 'Copy'
+    }, {
+        template: 'exampleHandle/exampleHandle.html',
+        title: 'Handle'
+    }, {
+        template: 'exampleCustomClasses/exampleCustomClasses.html',
+        title: 'Custom classes'
+    }, {
+        template: 'exampleNameSpaces/exampleNameSpaces.html',
+        title: 'NameSpaces'
+    }, {
+        template: 'exampleDragOverClasses/exampleDragOverClasses.html',
+        title: 'DragOver classes'
+    }, {
+        template: 'exampleBoundingBox/exampleBoundingBox.html',
+        title: 'BoundingBox'
+    }, {
+        template: 'exampleBoundingBoxLockX/exampleBoundingBoxLockX.html',
+        title: 'BoundingBox + LockX'
+    }, {
+        template: 'exampleBoundingBoxLockY/exampleBoundingBoxLockY.html',
+        title: 'BoundingBox + LockY'
+    }, {
+        template: 'exampleNgRepeat/exampleNgRepeat.html',
+        title: 'ngRepeat'
+    }, {
+        template: 'exampleNestedNgRepeat/exampleNestedNgRepeat.html',
+        title: 'Nested ngRepead'
+    }, {
+        template: 'exampleScrollingDrag/exampleScrollingDrag.html',
+        title: 'Scrolling drag'
+    }];
 
-  $rootScope.models = false;
-
-  $scope.toggleModels = function toggleModels() {
-    $rootScope.models = !$rootScope.models;
-  };
+    $rootScope.globals = {
+        showModelExamples: true
+    };
 }]);
 
 bulk(__dirname, ['./**/!(*App).js']);
