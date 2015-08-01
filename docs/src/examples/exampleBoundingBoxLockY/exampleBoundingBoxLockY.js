0c2be359edb7ed6adb5b5b1858c82670adfd1c8b
Philipp Alferov <palferov@securesense.ca>
2015-07-31 23:10:18 -0400

    Declare exampleBoundingBoxLockY using browserify

--- docs/src/examples/exampleBoundingBoxLockY/exampleBoundingBoxLockY.js
+++ docs/src/examples/exampleBoundingBoxLockY/exampleBoundingBoxLockY.js
@@ -1,6 +1,13 @@
 /* global angular */
 'use strict';
-angular.module('examplesApp')
+
+var examplesAppModule = require('../examplesApp');
+
+/**
+* @ngInject
+*/
+
+examplesAppModule
   .controller('Example12', ['$element', 'dragularService', function TodoCtrl($element, dragularService) {
     var boundingBox = $element.children().children()[0];
     dragularService(boundingBox, {
