172309a0a8e16f8697390f6c13d1230c6c0676ad
Ctibor Laky <luckylooke@gmail.com>
2017-03-10 06:11:28 +0100

    Update 2017-03-10T05:11:26.479Z

--- src/examples/exampleNgRepeatFilteredWithModel/exampleNgRepeatFilteredWithModel.js
+++ src/examples/exampleNgRepeatFilteredWithModel/exampleNgRepeatFilteredWithModel.js
@@ -2,47 +2,33 @@
 
 var NgRepeatFilteredWithModelCtrl = function ($scope, $element, dragularService, $filter) {
   $scope.items1 = [{
-    propty: {prop2: 'd'},
     content: 'Move me, but you can only drop me in one of these containers.'
   }, {
-    propty: {prop2: 'f'},
     content: 'If you try to drop me somewhere other than these containers, I\'ll just come back.'
   }, {
-    propty: {prop2: 'a'},
     content: 'Apple 3'
   }, {
-    propty: {prop2: 'c'},
     content: 'Orange 4'
   }, {
-    propty: {prop2: 'v'},
     content: 'Orange 5'
   }, {
-    propty: {prop2: 'h'},
     content: 'Apple 6'
   }, {
-    propty: {prop2: 'f'},
     content: 'Apple 7'
   }, {
-    propty: {prop2: 'k'},
     content: 'Apple 8'
   }];
   $scope.items2 = [{
-    propty: {prop2: 'u'},
     content: 'Apple 9'
   }, {
-    propty: {prop2: 'e'},
     content: 'Orange 10'
   }, {
-    propty: {prop2: 's'},
     content: 'Orange 11'
   }, {
-    propty: {prop2: 'u'},
     content: 'Apple 12'
   }, {
-    propty: {prop2: 'v'},
     content: 'Orange 13'
   }, {
-    propty: {prop2: 'n'},
     content: 'Apple 14'
   }];
   $scope.filter1query = 'Orange';
@@ -69,12 +55,12 @@ var NgRepeatFilteredWithModelCtrl = function ($scope, $element, dragularService,
     * So dragular will continue to use the one it was provided on init.
     * Hopefully I make it clear. :)
      */
-    // [].push.apply(filteredModel, $filter('filter')(items, filterQuery));
+    [].push.apply(filteredModel, $filter('filter')(items, filterQuery));
 
     // Example with orderBy filter:
-      var tmp = [];
-      [].push.apply(tmp, $filter('filter')(items, filterQuery));
-      [].push.apply(filteredModel, $filter('orderBy')(tmp, 'propty.prop2'));
+    //   var tmp = [];
+    //   [].push.apply(tmp, $filter('filter')(items, filterQuery));
+    //   [].push.apply(filteredModel, $filter('orderBy')(tmp, '+content'));
 
       return filteredModel;
   };
