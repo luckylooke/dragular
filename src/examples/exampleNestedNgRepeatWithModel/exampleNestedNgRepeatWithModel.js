599f167f0fa740ee7a2d8934e12b63e4ffc3f447
Ctibor Laky <luckylooke@gmail.com>
2019-01-22 06:33:53 +0100

    Update 2019-01-22T05:33:51.678Z

--- src/examples/exampleNestedNgRepeatWithModel/exampleNestedNgRepeatWithModel.js
+++ src/examples/exampleNestedNgRepeatWithModel/exampleNestedNgRepeatWithModel.js
@@ -2,9 +2,8 @@
 
 var NestedNgRepeatWithModelCtrl = function ($timeout, $scope, $element, dragularService) {
   $timeout(function() { // timeount due to nested ngRepeat to be ready
-    var rowsContainer = $element.children().eq(0).children(),
-      rows = getRows(),
-      cellContainers = getCellContainers();
+    var rowsContainer = $element[0].querySelector('.container-vertical'),
+      cellsContainers = getCellsContainers();
 
     dragularService.cleanEnviroment();
     dragularService(rowsContainer, {
@@ -15,11 +14,11 @@ var NestedNgRepeatWithModelCtrl = function ($timeout, $scope, $element, dragular
       nameSpace: 'rows'
     });
 
-    var cellContainersDrake = dragularService(cellContainers, {
+    var cellsContainersDrake = dragularService(cellsContainers, {
       moves: function(el, container, handle) {
         return handle.classList.contains('example-cell');
       },
-      containersModel: getcellContainersModel(),
+      containersModel: getcellsContainersModel(),
       nameSpace: 'cells'
     });
 
@@ -42,34 +41,22 @@ var NestedNgRepeatWithModelCtrl = function ($timeout, $scope, $element, dragular
         // $evalAsync is probably not enough to get after the DOM creation, so timeouted :/
         $timeout(function() {
           // you can provide all containers, dragular will add just new containers
-          cellContainersDrake.addContainers( getCellContainers(), getcellContainersModel());
+          cellsContainersDrake.addContainers( getCellsContainers(), getcellsContainersModel());
         }, 500);
       });
     }
 
     $scope.removeRow = function( row, e ) {
       var cellsContainerElm = e.target.parentElement.parentElement.querySelectorAll('.container-nested')[0];
-      cellContainersDrake.removeContainers( cellsContainerElm );
+      cellsContainersDrake.removeContainers( cellsContainerElm );
       $scope.rows.splice( $scope.rows.indexOf( row ), 1 );
     }
 
-    function getRows() {
-      return rowsContainer.children();
+    function getCellsContainers() {
+      return $element[0].querySelectorAll('.container-nested');
     }
 
-    function getCellContainers() {
-      var tmpContainers = [];
-      rows = getRows();
-      for (var i = 0; i < rows.length; i++) {
-        var cellContainer = rows.eq(i).children()[1];
-        if ( cellContainer ) {
-          tmpContainers.push( cellContainer );
-        }
-      }
-      return tmpContainers;
-    }
-
-    function getcellContainersModel(){
+    function getcellsContainersModel(){
         var parent = $scope.rows,
           containersModel = [];
         for (var i = 0; i < parent.length; i++) {
